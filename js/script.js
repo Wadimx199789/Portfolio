4032c24719a1485fc6775d5c86b08c2f6063b8a5
Wadim Martinkevich <martinkewichwadim@gmail.com>
2021-10-13 21:07:14 +0300

    feat: add sticky navigation menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 let cancelBtn = document.querySelector('.menu-cancel__btn');
 let openBtn = document.querySelector('.menu-open__btn');
-let navBar=document.querySelector('.navbar');
+let navBar = document.querySelector('.navbar');
 
 openBtn.addEventListener("click", function () {
     openBtn.style.opacity = "0";
     openBtn.style.pointerEvents = "none";
     navBar.classList.add("active");
 });
-cancelBtn.addEventListener("click",function(){
+cancelBtn.addEventListener("click", function () {
     openBtn.style.opacity = "1";
     openBtn.style.pointerEvents = "auto";
     navBar.classList.remove('active');
 });
+
+// Sticky navigation menu
+let nav = document.querySelector('nav');
+window.onscroll = function(){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("sticky");
+    }
+    else{
+        nav.classList.remove("sticky");
+    }
+}
